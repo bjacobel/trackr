5c92fa81988606f3d5205e82a66e22b5939785d9
Brian Jacobel <bjacobel@gmail.com>
2023-10-14 18:47:12 -0400

    Allow manifest to be fetched thru auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import './globals.css';
 import { Inter } from 'next/font/google';
 
@@ -6,12 +7,14 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
   title: 'Trackr',
   description: 'Track everything',
-  manifest: '/manifest.json',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
