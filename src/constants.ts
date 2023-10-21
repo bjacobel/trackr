8135b55b310835e3a6593cf4431297d7014b58b3
Brian Jacobel <bjacobel@gmail.com>
2023-10-21 16:58:11 -0400

    Use the production metric name in prod

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,6 @@ export const TRACKR_CATS = (process.env.NEXT_PUBLIC_TRACKD_RELATIVE || '').split
 export const HAS_CATS = !!TRACKR_CATS.length && !!TRACKR_CATS[0].length;
 export const OPENTSDB_HOST = process.env.OPENTSDB_HOST || 'localhost';
 export const OPENTSDB_PORT = process.env.OPENTSDB_PORT || 4242;
-export const TRACKR_RELATIVE_METRIC = 'dev_trackr_relative';
 export const PRODUCTION = process.env.NODE_ENV === 'production';
+export const TRACKR_RELATIVE_METRIC = `${PRODUCTION ? '' : 'dev_'}trackr_relative`;
+export const TRACKR_ABSOLUTE_METRIC = `${PRODUCTION ? '' : 'dev_'}trackr_absolute`;
