f8b208b556b3500120603eb4630621cd1a7bcc3b
Brian Jacobel <bjacobel@gmail.com>
2023-11-09 23:08:19 -0500

    Want white to appear behind new header

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 const manifest = (): MetadataRoute.Manifest => ({
   name: 'Trackr',
   short_name: 'Trackr',
-  theme_color: '#f5f5f5',
+  theme_color: '#ffffff',
   background_color: '#f5f5f5',
   display: 'standalone',
   orientation: 'portrait',
